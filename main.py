501d64d2ab6d465c3f7a7d4195a4e5c45a5db144
datarec <73265375+datarec@users.noreply.github.com>
2024-12-08 19:21:29 +1100

    Update main.py

--- main.py
+++ main.py
@@ -6,19 +6,23 @@ import tkinter as tk
 import os
 import pathlib
 
-
-def open_window():
+def test(cat):
+    cats = str(cat).split("C:\\Users\\h0hr\\Scripts\\misc\\advertisement\\kdo\\")
     root = Tk()
-    cat_path = pathlib.Path("C:\\Users\\h0hr\\Scripts\\misc\\advertisement\\cats")
-    for cats in cat_path.iterdir():
-        root = Tk()
-        str_cat = str(cats)
-        str_cat_filename = str_cat.strip("C:\\Users\\h0hr\\Scripts\\misc\\advertisement\\cats\\")
-        print(str_cat_filename)
-    cat_picture = PhotoImage(file="{}".format(str_cat_filename))
+    cat_picture = PhotoImage(file="{}".format(cats[1]))
     cat = Label(root, image=cat_picture)
     cat.pack()
     root.mainloop()
+
+
+
+def open_window(): 
+    cat_path = pathlib.Path(r"C:\Users\h0hr\Scripts\misc\advertisement\kdo")
+    os.chdir(cat_path)
+    for cat in cat_path.iterdir():
+        cats = str(cat).split("C:\\Users\\h0hr\\Scripts\\misc\\advertisement\\kdo\\")
+        cat_thread = threading.Thread(target=test, kwargs={"cat":"catd"})
+        cat_thread.start()
 
 
 def main():
@@ -30,13 +34,7 @@ def main():
                 cat_window.start()
     except KeyboardInterrupt:
         exit()
-
- 
-
-def copy_operation():
-    pass
     
-
 
 if __name__ == "__main__":
     main()
